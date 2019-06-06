2c1ae98137bf029b609258f8c20a771616502639
Danton Moura <dantonmoura@hotmail.com>
2019-06-06 12:02:32 -0300

    estrutura finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dantonton';
-
+  layoutAlign = 'center center';
   select_sessao = 'inativo';
 
-  clickEvent(){
-    this.select_sessao = 'asdad';       
+  selecionado = 0;
+
+  clickEvent(sel:number){
+    if(sel == 0 || sel == this.selecionado){
+      this.layoutAlign ='center center';
+      this.select_sessao = 'inativo';
+      this.selecionado = 0;
+      return;
+
+    }else if(sel == 1){
+      this.layoutAlign ='end end';
+      this.select_sessao = 'ativo-lt';
+    }else if(sel == 2){
+      this.layoutAlign ='start end';
+      this.select_sessao = 'ativo-rt';
+    }else if(sel == 3){
+      this.layoutAlign ='end start';
+      this.select_sessao = 'ativo-rb';
+    }else if(sel == 4){
+      this.layoutAlign ='start start';
+      this.select_sessao = 'ativo-lb';
+    }
+
+    this.selecionado = sel;
+    //this.select_sessao = 'asdad';       
   }
 }
