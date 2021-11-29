3bc57ce49263e3f9efc78a4651d7bbf897bf26aa
Danton Ribeiro <danton.ribeiro@championx.com>
2021-11-29 02:18:57 -0300

    versão 2.0 agora vai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Lightbox } from 'ngx-lightbox';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'dantonton';
   yearRalative = (new Date()).getFullYear() - 2011;
 
@@ -13,6 +15,36 @@ export class AppComponent {
   serviceSelected = ['','',''];
   portifolio = 0;
 
+  portifolioList: {title:string, body: string, img: string[] }[] = [
+    {title : 'Ambev',
+  body : "Gerenciamento de questionários para pesquisa de opinião. Arquiteturei o projeto para permitir diversos tipos de perguntas, todo de forma genéria e orientada a objetos.",
+  img : ['../assets/port/ambev.png']},
+  {title : 'White Martins',
+  body : "Ferramenta para o grupo White Martins calcular orçamentos. Ajudei na manutenção do software e na contrução de um ambiente de entrega continua e um código manutenível",
+  img : ['../assets/port/whitemartins.png']},
+  {title : 'FAPES e NIADES',
+  body : "Arquiteturei um sitema web SaaS para diversos comites de bacía hidrográfica.",
+  img : ['../assets/port/fapes.png']},
+  {title : 'Unimed',
+  body : "Desenvolvimento de aplicativo híbrido para Unimed. Seguindo a especificação de desing com precisão.",
+  img : ['../assets/port/unnamed.webp', '../assets/port/unnamed_.webp', '../assets/port/unnamed_2.webp']},
+  {title : 'MDK Exames',
+  body : "Aplicativo de web scraping de exames laboratoriais",
+  img : ['../assets/port/mdk.webp', '../assets/port/mdk_2.webp']},
+  {title : 'Solverup',
+  body : "Criação do website https://www.solverup.com.br/ com sistema de registro de demanda.",
+  img : ['../assets/port/solverup.png']},
+  {title : 'SelfStyle',
+  body : "Criação e design do website https://selfstyle.online/",
+  img : ['../assets/port/selfstyle.png']},
+  ];
+
+  constructor(private _lightbox: Lightbox) {
+  }
+  openPort(port: {title:string, body: string, img: string[] }, index: number){
+
+    this._lightbox.open(port.img.map(x=>({src : x,thumb: x})), index);
+  }
   activeSessionSelected(indexActive: number){
     for (let index = 0; index < this.serviceSelected.length; index++) {
       if(indexActive == index){
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule} from '@angular/flex-layout';
 import {MatToolbarModule} from '@angular/material';
 import { MatCardModule  } from '@angular/material';
-import {MatIconModule} from '@angular/material/icon';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
 import { GalleryModule } from '@ngx-gallery/core';
-
+import { LightboxModule } from 'ngx-lightbox';
 import { AppComponent } from './app.component';
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
@@ -28,7 +29,9 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     MatToolbarModule,
     MatCardModule,
     MatIconModule,
-    PerfectScrollbarModule
+    CommonModule,
+    PerfectScrollbarModule,
+    LightboxModule
   ],
   providers: [
     {
